53610f16be6e5dbd9d94672e044fbca29412b612
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-07-12 15:05:11 +0300

    fixi config

--- init.lua
+++ init.lua
@@ -871,7 +871,7 @@ do
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
-      selection = { preselect = true, auto_insert = true }
+      list = { selection = { preselect = true, auto_insert = true } },
     },
 
     sources = {
