050199ae076b5ae7daa14e2c7aac1f2da2b070bb
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-07-12 14:02:09 +0300

    adjust dashboard config

--- lua/kickstart/plugins/dashboard.lua
+++ lua/kickstart/plugins/dashboard.lua
@@ -8,18 +8,20 @@ require('snacks').setup {
     enabled = true,
     preset = {
       keys = {
-        { icon = ' ', key = 'f', desc = 'Find File', action = function() Snacks.dashboard.pick 'files' end },
-        { icon = ' ', key = 'r', desc = 'Recent Files', action = function() Snacks.dashboard.pick 'oldfiles' end },
-        { icon = ' ', key = 'g', desc = 'Find Word', action = function() Snacks.dashboard.pick 'live_grep' end },
-        { icon = ' ', key = 'e', desc = 'File Browser', action = '<Cmd>Neotree reveal<CR>' },
-        { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+        { icon = ' ', key = 'f', desc = 'Find File', action = function() Snacks.dashboard.pick 'files' end },
+        { icon = ' ', key = 'r', desc = 'Recent Files', action = function() Snacks.dashboard.pick 'oldfiles' end },
+        { icon = ' ', key = 'g', desc = 'Find Word', action = function() Snacks.dashboard.pick 'live_grep' end },
+        { icon = ' ', key = 'e', desc = 'File Browser', action = '<Cmd>Neotree reveal<CR>' },
+        { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
       },
     },
-    -- Default preset also includes a `startup` section, which assumes
-    -- lazy.nvim (for load-time stats) and errors under vim.pack -- omit it.
+    -- "compact_files" example from the docs, minus the `startup` section,
+    -- which assumes lazy.nvim (for load-time stats) and errors under vim.pack.
     sections = {
       { section = 'header' },
-      { section = 'keys', gap = 1, padding = 1 },
+      { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+      { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+      { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
     },
   },
 }
